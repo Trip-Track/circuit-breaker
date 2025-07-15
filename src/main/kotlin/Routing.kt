778131b51eea3dc5feb35879f1fd34e8b880312c
hanzlvoj <hanzlvoj@fel.cvut.cz>
2025-07-15 22:30:31 +0200

    more routes

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -9,25 +9,58 @@ import swa.circuit_breaker.CircuitBreaker
 import swa.circuit_breaker.CircuitBreakerReject
 
 
+
 fun Application.configureRouting() {
 
-    val circuitBreaker = CircuitBreaker(
-        baseUrl = System.getenv("WEATHER_URL") ?: "http://weather:8000"
+    val tripPlannerCB = CircuitBreaker(
+        baseUrl = System.getenv("TRIP_PLANNER_URL") ?: "http://trip-planner:8000"
+    )
+    val cityInfoCB = CircuitBreaker(
+        baseUrl = System.getenv("CITY_INFO_URL") ?: "http://city-info:8000"
     )
 
     routing {
-        get("/weather") {
 
+        route("/trip") {
+            get {
+                try {
+                    tripPlannerCB.routeRequest(call.request.path(), call.request.queryParameters)
+                    call.respond("")                    // body already streamed back
+                } catch (_: CircuitBreakerReject) {
+                    call.respond(HttpStatusCode.ServiceUnavailable,
+                        "Trip-planner temporarily unavailable")
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                    call.respond(HttpStatusCode.BadGateway)
+                }
+            }
+        }
+
+        get("/map") {
             try {
-                circuitBreaker.routeRequest(call.request.path(), call.request.queryParameters)
+                tripPlannerCB.routeRequest(call.request.path(), call.request.queryParameters)
                 call.respond("")
             } catch (_: CircuitBreakerReject) {
                 call.respond(HttpStatusCode.ServiceUnavailable,
-                    "Weather temporarily unavailable")
+                    "Trip-planner temporarily unavailable")
             } catch (ex: Exception) {
-                println(ex.message)
-                println(ex)
+                ex.printStackTrace()
                 call.respond(HttpStatusCode.BadGateway)
+            }
+        }
+
+        route("/city/{city}/{endpoint}") {
+            get {
+                try {
+                    cityInfoCB.routeRequest(call.request.path(), call.request.queryParameters)
+                    call.respond("")
+                } catch (_: CircuitBreakerReject) {
+                    call.respond(HttpStatusCode.ServiceUnavailable,
+                        "City-info temporarily unavailable")
+                } catch (ex: Exception) {
+                    ex.printStackTrace()
+                    call.respond(HttpStatusCode.BadGateway)
+                }
             }
         }
     }
