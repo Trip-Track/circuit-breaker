9487eee019d66e21fa6cb91b113cc7f271ff6b0c
hanzlvoj <hanzlvoj@fel.cvut.cz>
2025-07-29 09:20:39 +0200

    delete unused code

--- src/main/kotlin/Routing.kt
+++ src/main/kotlin/Routing.kt
@@ -8,9 +8,7 @@ import io.ktor.server.application.*
 import io.ktor.server.request.path
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
-import io.ktor.utils.io.copyTo
 import io.ktor.utils.io.jvm.javaio.copyTo
-import swa.UrlResolver.discoverService
 import swa.circuit_breaker.CircuitBreaker
 import swa.circuit_breaker.CircuitBreakerReject
 
